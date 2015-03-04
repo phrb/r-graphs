cce38ef1ebe5c84e740bef6bc06fb8d27b836314
Pedro Bruel <pedro.bruel@gmail.com>
2015-03-04 13:23:20 -0300

    Directory Hierarchy, 12htuning data.

--- plot.R
+++ plot.R
@@ -3,48 +3,67 @@
 library(ggplot2)
 library(reshape2)
 
-log_all <- read.table("logall.txt",header=TRUE)
-log_bst <- read.table("bestlog.txt",header=TRUE)
-solver_data <- read.table("solver_data.txt",header=TRUE)
-solver_short_data <- read.table("solver_short_data.txt",header=TRUE)
-tuned_data <- read.table("solver_tuned.txt",header=TRUE)
+log_all_24 <- read.table("24_short/logall.txt",header=TRUE)
+log_bst_24 <- read.table("24_short/logbest.txt",header=TRUE)
 
-graph_all <- qplot(log_all$tuning_time,log_all$run_time,data=log_all,
-                   xlab="Execution Time (seconds)",
-                   ylab="Tuning Time (seconds)", 
+log_all_12 <- read.table("12_all/logall.txt",header=TRUE)
+log_bst_12 <- read.table("12_all/logbest.txt",header=TRUE)
+
+old_benchmark <- read.table("benchmarks/old_benchmark.txt",header=TRUE)
+old_short_benchmark <- read.table("benchmarks/old_short_benchmark.txt",header=TRUE)
+
+tuned_comparison24 <- read.table("24_short/comparison.txt",header=TRUE)
+
+graph_all_24 <- qplot(log_all_24$tuning_time,log_all_24$run_time,data=log_all_24,
+                   ylab="Execution Time (seconds)",
+                   xlab="Tuning Time (seconds)", 
                    main="Tuning Time vs. Execution Time over 24h")
 
-graph_bst <- qplot(log_bst$tuning_time,log_bst$run_time,
+graph_bst_24 <- qplot(log_bst_24$tuning_time,log_bst_24$run_time,
                    geom=c("point","line"),
-                   xlab="Execution Time (seconds)",
-                   ylab="Tuning Time (seconds)",      
+                   ylab="Execution Time (seconds)",
+                   xlab="Tuning Time (seconds)",      
                    main="Tuning Time vs. Best Execution Time over 24h")
 
-graph_solver <- ggplot(data=melt(solver_data), aes(x=variable,y=value)) +
+graph_all_12 <- qplot(log_all_12$tuning_time,log_all_12$run_time,data=log_all_12,
+                   ylab="Execution Time (seconds)",
+                   xlab="Tuning Time (seconds)", 
+                   main="Tuning Time vs. Execution Time over 12h")
+
+graph_bst_12 <- qplot(log_bst_12$tuning_time,log_bst_12$run_time,
+                   geom=c("point","line"),
+                   ylab="Execution Time (seconds)",
+                   xlab="Tuning Time (seconds)",      
+                   main="Tuning Time vs. Best Execution Time over 12h")
+graph_old_bench <- ggplot(data=melt(old_benchmark), aes(x=variable,y=value)) +
     geom_boxplot(aes(fill=variable)) +
     xlab("Solvers") +
     ylab("Execution Time (seconds)") +
     ggtitle("Execution Times Over 80 runs vs Solvers")
 
-graph_short_solver <- ggplot(data=melt(solver_short_data), aes(x=variable,y=value)) +
+graph_old_short <- ggplot(data=melt(old_short_benchmark), aes(x=variable,y=value)) +
     geom_boxplot(aes(fill=variable)) +
     xlab("Solvers") +
     ylab("Execution Time (seconds)") +
     ggtitle("Execution Times Over 80 runs vs Solvers")
 
-graph_tuned_solver <- ggplot(data=melt(tuned_data), aes(x=variable,y=value)) +
+graph_tuned_comparison24 <- ggplot(data=melt(tuned_comparison24), aes(x=variable,y=value)) +
     geom_boxplot(aes(fill=variable)) +
     xlab("Solvers") +
     ylab("Execution Time (seconds)") +
     ggtitle("Execution Times Over 80 runs vs Solvers")
 
-print(graph_all)
+print(graph_all_24)
 readline("plot done\n")
-print(graph_bst)
+print(graph_bst_24)
 readline("plot done\n")
-print(graph_solver)
+print(graph_all_12)
 readline("plot done\n")
-print(graph_short_solver)
+print(graph_bst_12)
 readline("plot done\n")
-print(graph_tuned_solver)
+print(graph_old_bench)
+readline("plot done\n")
+print(graph_old_short)
+readline("plot done\n")
+print(graph_tuned_comparison24)
 readline("plot done\n")
