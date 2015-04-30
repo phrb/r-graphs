7954f03a880ecb5705b8468ff7b0ccec63c07bcf
phrb <pedro.bruel@gmail.com>
2015-04-30 13:08:17 -0300

    Improved rendering on old encoding graph.

--- sat-opentuner/old_encoding/plot.R
+++ sat-opentuner/old_encoding/plot.R
@@ -6,7 +6,7 @@ library(reshape2)
 #
 # Loading Files:
 #
-#log_all_24 <- read.table("24_short/logall.txt",header=TRUE)
+log_all_24 <- read.table("24_short/logall.txt",header=TRUE)
 #log_bst_24 <- read.table("24_short/logbest.txt",header=TRUE)
 #
 #log_all_12 <- read.table("12_all/logall.txt",header=TRUE)
@@ -23,7 +23,7 @@ library(reshape2)
 #
 #real_benchmark <- read.table("benchmarks/benchmark_real/benchmark.txt",header=TRUE)
 #real_short <- read.table("benchmarks/benchmark_real/benchmark_short.txt",header=TRUE)
-#
+##
 #tuned_comparison24 <- read.table("24_short/comparison.txt",header=TRUE)
 #real_tuned_comparison24 <- read.table("24_short/real_comparison.txt",header=TRUE)
 #faster_tuned_comparison24 <- read.table("24_short/faster_comparison.txt",header=TRUE)
@@ -33,139 +33,144 @@ library(reshape2)
 #
 # Uncomment to generate all files.
 #
-graph_all_24 <- qplot(log_all_24$tuning_time,log_all_24$run_time,data=log_all_24,
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)", 
-                   main="Tuning Time (Only Best Solvers) vs. Execution Time over 24h (Measured by OpenTuner)")
+#graph_all_24 <- qplot(log_all_24$tuning_time,log_all_24$run_time,data=log_all_24,
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)", 
+#                   main="Tuning Time (Only Best Solvers) vs. Execution Time over 24h (Measured by OpenTuner)")
 
-ggsave(file="log_all_24.png",width=7,height=4,scale=2)
+#ggsave(file="log_all_24.png",width=7,height=4,scale=2)
 
-graph_bst_24 <- qplot(log_bst_24$tuning_time,log_bst_24$run_time,
-                   geom=c("point","line"),
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)",      
-                   main="Tuning Time (Only Best Solvers) vs. Best Execution Time over 24h (Measured by OpenTuner)")
+graph_all_24_2 <- ggplot(log_all_24, aes(tuning_time, run_time)) +
+                    geom_point(aes(colour=run_time)) +
+                    scale_colour_gradientn(colours=rainbow(3))
+ggsave(file="log_all_24_2.png",width=7,height=4,scale=2)
 
-ggsave(file="log_bst_24.png",width=7,height=4,scale=2)
+#graph_bst_24 <- qplot(log_bst_24$tuning_time,log_bst_24$run_time,
+#                   geom=c("point","line"),
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)",      
+#                   main="Tuning Time (Only Best Solvers) vs. Best Execution Time over 24h (Measured by OpenTuner)")
 
-graph_all_12 <- qplot(log_all_12$tuning_time,log_all_12$run_time,data=log_all_12,
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)", 
-                   main="Tuning Time (All Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
+#ggsave(file="log_bst_24.png",width=7,height=4,scale=2)
 
-ggsave(file="log_all_12.png",width=7,height=4,scale=2)
+#graph_all_12 <- qplot(log_all_12$tuning_time,log_all_12$run_time,data=log_all_12,
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)", 
+#                   main="Tuning Time (All Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
 
-graph_bst_12 <- qplot(log_bst_12$tuning_time,log_bst_12$run_time,
-                   geom=c("point","line"),
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)",      
-                   main="Tuning Time (All Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
-
-ggsave(file="log_bst_12.png",width=7,height=4,scale=2)
-
-graph_all_12_headstart <- qplot(log_all_12_headstart$tuning_time,log_all_12_headstart$run_time,data=log_all_12_headstart,
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)", 
-                   main="Tuning Time (With Headstart, Only Best Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
-
-ggsave(file="log_all_12_headstart.png",width=7,height=4,scale=2)
-
-graph_bst_12_headstart <- qplot(log_bst_12_headstart$tuning_time,log_bst_12_headstart$run_time,
-                   geom=c("point","line"),
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)",      
-                   main="Tuning Time (With Headstart, Only Best Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
-
-ggsave(file="log_bst_12_headstart.png",width=7,height=4,scale=2)
-
-graph_all_12_beststart <- qplot(log_all_12_beststart$tuning_time,log_all_12_beststart$run_time,data=log_all_12_beststart,
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)", 
-                   main="Tuning Time (With Beststart, Only Best Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
-
-ggsave(file="log_all_12_beststart.png",width=7,height=4,scale=2)
-
-graph_bst_12_beststart <- qplot(log_bst_12_beststart$tuning_time,log_bst_12_beststart$run_time,
-                   geom=c("point","line"),
-                   ylab="Execution Time (seconds)",
-                   xlab="Tuning Time (seconds)",      
-                   main="Tuning Time (With Beststart, Only Best Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
-
-ggsave(file="log_bst_12_beststart.png",width=7,height=4,scale=2)
-
-graph_old_bench <- ggplot(data=melt(old_benchmark), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs vs All Solvers (/usr/bin/time: sys+usr)")
-
-ggsave(file="sys_usr_all_benchmark.png",width=7,height=4,scale=2)
-
-graph_old_short <- ggplot(data=melt(old_short_benchmark), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs vs Best Solvers (/usr/bin/time: sys+usr)")
-
-ggsave(file="sys_usr_bst_benchmark.png",width=7,height=4,scale=2)    
-
-graph_real_bench <- ggplot(data=melt(real_benchmark), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs vs All Solvers (/usr/bin/time: real)")
-
-ggsave(file="real_all_benchmark.png",width=7,height=4,scale=2)    
-
-graph_real_short <- ggplot(data=melt(real_short), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs vs Best Solvers (/usr/bin/time: real)")
-
-ggsave(file="real_bst_benchmark.png",width=7,height=4,scale=2)
-
-graph_tuned_comparison24 <- ggplot(data=melt(tuned_comparison24), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: sys+usr)")
-
-ggsave(file="sys_usr_tuned_comparison_24.png",width=7,height=4,scale=2)
-
-graph_real_comparison24 <- ggplot(data=melt(real_tuned_comparison24), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: real)")
-
-ggsave(file="real_tuned_comparison_24.png",width=7,height=4,scale=2)
-
-graph_faster_comparison24 <- ggplot(data=melt(faster_tuned_comparison24), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: real)")
-
-ggsave(file="faster_real_tuned_comparison_24.png",width=7,height=4,scale=2)
-
-graph_headstart_comparison12 <- ggplot(data=melt(headstart_comparison12), aes(x=variable,y=value)) +
-    geom_boxplot(aes(fill=variable)) +
-    xlab("Solvers") +
-    ylab("Execution Time (seconds)") +
-    ggtitle("Execution Times Over 80 runs (12h) vs Best Solvers (/usr/bin/time: real)")
-
-ggsave(file="headstart_comparison_24.png",width=7,height=4,scale=2)
-
+#ggsave(file="log_all_12.png",width=7,height=4,scale=2)
 #
-# Uncomment for visualization in R interpreter.
+#graph_bst_12 <- qplot(log_bst_12$tuning_time,log_bst_12$run_time,
+##                   geom=c("point","line"),
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)",      
+#                   main="Tuning Time (All Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
 #
-#print(graph_all_24)
-#readline("plot done\n")
-#print(graph_all_12)
-#readline("plot done\n")
-#print(graph_bst_24)
-#readline("plot done\n")
+#ggsave(file="log_bst_12.png",width=7,height=4,scale=2)
+#
+#graph_all_12_headstart <- qplot(log_all_12_headstart$tuning_time,log_all_12_headstart$run_time,data=log_all_12_headstart,
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)", 
+##                   main="Tuning Time (With Headstart, Only Best Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
+#
+#ggsave(file="log_all_12_headstart.png",width=7,height=4,scale=2)
+#
+#graph_bst_12_headstart <- qplot(log_bst_12_headstart$tuning_time,log_bst_12_headstart$run_time,
+#                   geom=c("point","line"),
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)",      
+#                   main="Tuning Time (With Headstart, Only Best Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
+#
+#ggsave(file="log_bst_12_headstart.png",width=7,height=4,scale=2)
+#
+#graph_all_12_beststart <- qplot(log_all_12_beststart$tuning_time,log_all_12_beststart$run_time,data=log_all_12_beststart,
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)", 
+#                   main="Tuning Time (With Beststart, Only Best Solvers) vs. Execution Time over 12h (Measured by OpenTuner)")
+#
+#ggsave(file="log_all_12_beststart.png",width=7,height=4,scale=2)
+#
+#graph_bst_12_beststart <- qplot(log_bst_12_beststart$tuning_time,log_bst_12_beststart$run_time,
+#                   geom=c("point","line"),
+#                   ylab="Execution Time (seconds)",
+#                   xlab="Tuning Time (seconds)",      
+#                   main="Tuning Time (With Beststart, Only Best Solvers) vs. Best Execution Time over 12h (Measured by OpenTuner)")
+#
+#ggsave(file="log_bst_12_beststart.png",width=7,height=4,scale=2)
+#
+#graph_old_bench <- ggplot(data=melt(old_benchmark), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs vs All Solvers (/usr/bin/time: sys+usr)")
+#
+#ggsave(file="sys_usr_all_benchmark.png",width=7,height=4,scale=2)
+#
+#graph_old_short <- ggplot(data=melt(old_short_benchmark), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs vs Best Solvers (/usr/bin/time: sys+usr)")
+#
+#ggsave(file="sys_usr_bst_benchmark.png",width=7,height=4,scale=2)    
+#
+#graph_real_bench <- ggplot(data=melt(real_benchmark), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs vs All Solvers (/usr/bin/time: real)")
+#
+#ggsave(file="real_all_benchmark.png",width=7,height=4,scale=2)    
+#
+#graph_real_short <- ggplot(data=melt(real_short), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs vs Best Solvers (/usr/bin/time: real)")
+#
+#ggsave(file="real_bst_benchmark.png",width=7,height=4,scale=2)
+#
+#graph_tuned_comparison24 <- ggplot(data=melt(tuned_comparison24), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: sys+usr)")
+#
+#ggsave(file="sys_usr_tuned_comparison_24.png",width=7,height=4,scale=2)
+#
+#graph_real_comparison24 <- ggplot(data=melt(real_tuned_comparison24), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: real)")
+#
+#ggsave(file="real_tuned_comparison_24.png",width=7,height=4,scale=2)
+#
+#graph_faster_comparison24 <- ggplot(data=melt(faster_tuned_comparison24), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs (24h) vs Best Solvers (/usr/bin/time: real)")
+#
+#ggsave(file="faster_real_tuned_comparison_24.png",width=7,height=4,scale=2)
+#
+#graph_headstart_comparison12 <- ggplot(data=melt(headstart_comparison12), aes(x=variable,y=value)) +
+#    geom_boxplot(aes(fill=variable)) +
+#    xlab("Solvers") +
+#    ylab("Execution Time (seconds)") +
+#    ggtitle("Execution Times Over 80 runs (12h) vs Best Solvers (/usr/bin/time: real)")
+#
+#ggsave(file="headstart_comparison_24.png",width=7,height=4,scale=2)
+#
+##
+## Uncomment for visualization in R interpreter.
+##
+##print(graph_all_24)
+##readline("plot done\n")
+##print(graph_all_12)
+##readline("plot done\n")
+##print(graph_bst_24)
+##readline("plot done\n")
 #print(graph_bst_12)
 #readline("plot done\n")
 #print(graph_old_bench)
